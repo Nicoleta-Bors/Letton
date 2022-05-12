02b6fffcb2309a01818e3851da118930fa3c7e53
Bors Nicoleta <nicutabors@gmail.com>
2022-05-12 23:00:52 +0300

    Final commit

---
+++ Letton.Domain/Entities/Product/ProductDetailData.cs
@@ -0,0 +1,13 @@
+﻿using Letton.Domain.Enums;
+
+namespace Letton.Domain.Entities.Product
+{
+     public class ProductDetailData
+     {
+          public int Id { get; set; }
+          public string UserName { get; set; }
+          public string Email { get; set; }
+          public URole Level { get; set; }
+          public ProductsDbTable Product { get; set; }
+     }
+}
